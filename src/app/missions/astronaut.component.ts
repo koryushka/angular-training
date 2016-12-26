151130d31d8c82cfddb038ff308024303f6eedfd
Kiryl Karetnikau <kiryl_karetnikau@epam.com>
2016-12-26 15:26:52 +0300

    Removed subscription

--- src/app/missions/astronaut.component.ts
+++ src/app/missions/astronaut.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { MissionService } from './mission.service';
-import { Subscription }   from 'rxjs/Subscription';
 @Component({
   selector: 'astronaut',
   templateUrl: './astronaut.component.html'
@@ -11,10 +10,9 @@ export class AstronautComponent{
   mission = '<No mission announced>'
   confirmed = false;
   announced = false;
-  subscription: Subscription;
 
   constructor(private missionService: MissionService) {
-    this.subscription = missionService.missionAnnounced$.subscribe(
+    missionService.missionAnnounced$.subscribe(
       mission => {
         this.mission = mission;
         this.announced = true;
