b860c9408da12233e21108a578bb1bf16b4a60eb
Kiryl Karetnikau <kiryl_karetnikau@epam.com>
2016-12-27 16:34:00 +0300

    Ready to deploy

--- src/app/missions/mission.service.ts
+++ src/app/missions/mission.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Subject }    from 'rxjs/Subject';
+import { Observable }    from 'rxjs/Observable';
+
 
 @Injectable()
 export class MissionService {
